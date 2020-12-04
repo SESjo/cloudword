4349de51d59b2edafb4acc0cc653adfd213f68e1
Joffrey JOUMA'A <joffreyjoumaa@icloud.com>
2020-12-04 18:09:22 +0100

    changement de la méthode d'imputation des fréquences

--- src/import.R
+++ src/import.R
@@ -17,12 +17,21 @@ library(data.table)
 dataset = setDT(read.csv("data/mots.csv"))
 
 # duplicate
-dataset = do.call("rbind", replicate(20, dataset, simplify = FALSE))
-data_inter = data.table(freq = (1/log(c(1:101)))[-1],
-                        pt = seq(1, 2000, by = 20))
+x_rep = 30
+x_seq = x_rep * 100
+dataset = do.call("rbind", replicate(x_rep, dataset, simplify = FALSE))
+
+# logarithm
+# data_inter = data.table(freq = (1/log(c(1:101)))[-1],
+#                         pt = seq(1, x_seq, by = x_rep))
+
+# linear
+data_inter = data.table(freq = seq(1, 0, length.out = 100),
+                        pt = seq(1, x_seq, by = x_rep))
+
 resultat=approx(data_inter$pt, 
                 data_inter$freq, 
-                xout = seq(1, 2000, by = 1), 
+                xout = seq(1, x_seq, by = 1), 
                 method="linear", 
                 ties="ordered")$y
 dataset[, freq := resultat[1:nrow(dataset)]]
